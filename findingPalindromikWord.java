16ef24539808a467efbeee13c1e1eee8f75a23ac
Merve <61613791+merveozderin@users.noreply.github.com>
2022-01-10 12:19:42 +0300

    Add files via upload

---
+++ findingPalindromikWord.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class findingPalindromikWord
+{
+        static boolean isPalindrome(String str) {
+            int i = 0, j = str.length() - 1;
+            while (i < j) {
+                if (str.charAt(i) != str.charAt(j))
+                    return false;
+                i++;
+                j--;
+            }
+            return true;
+        }
+
+        static boolean isPalindrome2(String str) {
+            String reverse = "";
+            for (int i = str.length() - 1; i >= 0; i--) {
+                reverse += str.charAt(i);
+            }
+
+            if (str.equals(reverse))
+                return true;
+            else
+                return false;
+        }
+
+        public static void main(String[] args)
+        {
+            Scanner input_value = new Scanner(System.in);
+            String st;
+            System.out.println("Given a word: ");
+            st = input_value.nextLine();
+            System.out.println(isPalindrome(st));
+        }
+}
+
